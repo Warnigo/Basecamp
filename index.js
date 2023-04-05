23932cfb4e94d81f70925de840719a17a9bdbd0e
WARNIGO <119101655+warniGO@users.noreply.github.com>
2023-04-05 11:14:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 app.set('view engine', 'hbs');  
-
 app.use('/', require('./routes/page'));
+app.use(express.static('.'));
 
-app.listen(3000, () => {
-    console.log('Server has been started on 3000...');
-}); 
+app.listen(4000, () => {
+    console.log('Server has been started on 4000...');
+});
+
+
