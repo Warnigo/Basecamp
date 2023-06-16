e6140f9a89dca0536dbebaaa8921cc5a28049fd2
Abubakir Shavkatov <119101655+warniGO@users.noreply.github.com>
2023-06-16 21:25:30 +0500

    Update page.js

--- routes/page.js
+++ routes/page.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
 
 router.get('/', (req, res) => res.render('home'));
 router.get('/register', (req, res) => res.render('register'));
 router.get('/login', (req, res) => res.render('login'));
+router.get('/logout', (req, res) => res.render('logout'));
+
+router.get('/profile', (req, res) => res.render('profile'));
 
 module.exports = router;
